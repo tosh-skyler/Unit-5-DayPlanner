e3b116275d29e6c07945abd3993844a19163cfb8
Skyler Tosh <skylerjtosh@gmail.com>
2020-02-05 01:05:26 -0800

    scripts.js now writes the time blocks to the #section-a container

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,5 +2,27 @@ var dateString = moment().format("dddd, MMMM Do YYYY, h:mm a");
 $("#currentDay").html(dateString);
 
 var timeString = moment().format("h A");
-$("#currentHour").html(timeString);
 
+var scheduledHours = [];
+
+for (var hour = 9; hour < 18; hour++) {
+  scheduledHours.push(
+    moment({
+      hour
+    }).format("h  a")
+  );
+  $("#section-a").append(`
+		<div class="row time-block" data-time="${hour}"> 
+			<!--hour column-->
+			<div class="hour">
+				<span class="time-block">${moment({ hour }).format("h  a")}</span>
+			</div>
+			<!--user input text area-->
+			<textarea class="description" id="${moment({ hour }).format("HH")}"></textarea>
+			<!-- saveBtn-->
+			<button class="saveBtn">
+				<i class="fas fa-save"></i>
+			</button>
+		</div>`);
+  console.log({ hour });
+}
